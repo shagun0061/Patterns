c13c71eb00252fe0c6c293f0a065c4f646fdfc98
shagun <shagun08081999@gmail.com>
2022-11-11 01:19:33 +0530

    Prototype inhetience

--- prtotype-iheritance/proto.js
+++ prtotype-iheritance/proto.js
@@ -1,25 +1,21 @@
-// !     Create Prototype 
-
+// !     Create Prototype
 
 Object.prototype.mylength=function(){
-    
+
     console.log(this.name + "lolol");
 }
 
 function fun1(){
-    
+
 }
 let obj={
     name:"sjahgi"
 }
 
-
-
 // !     Prototype inhetience
 
-
 function fun1(){
-    
+
 }
 let obj1={
     name:"sjahgi"
